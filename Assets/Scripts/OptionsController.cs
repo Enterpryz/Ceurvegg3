ea14014bc75c08f328021854e4adff9e8c0c3de7
Enterpryz <enterpryz1@gmail.com>
2020-05-08 19:17:45 -0400

    169 - Set And Save Music Volume

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float fltDefaultVolume = 0.8f;
+
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("START FROM SPLASH SCREEN OR NO MUSIC PLAYER.");
+        }
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = fltDefaultVolume;
+    }
+
+
+}

85cc75fbc2bf72bbc86a988c40f49f32122aae29
Enterpryz <enterpryz1@gmail.com>
2020-05-08 18:32:07 -0400

    167 - Start Our Options Screen

---
+++ Assets/Scripts/TEST.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class TEST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefsController.SetMasterVolume(0.4f);
+        UnityEngine.Debug.Log(PlayerPrefsController.GetMasterVolume());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
